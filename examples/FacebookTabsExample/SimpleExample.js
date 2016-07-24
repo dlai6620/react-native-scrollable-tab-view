15f21d47b4706b33f55620050d046b7a9c91569d
dlai6620 <dlai6620@uni.sydney.edu.au>
2016-07-24 21:10:33 +1000

    Update SimpleExample.js

--- examples/FacebookTabsExample/SimpleExample.js
+++ examples/FacebookTabsExample/SimpleExample.js
@@ -7,13 +7,15 @@ import ScrollableTabView, {DefaultTabBar} from 'react-native-scrollable-tab-view
 
 export default React.createClass({
   render() {
-    return <ScrollableTabView
+    return (
+    <ScrollableTabView
       style={{marginTop: 20, }}
       renderTabBar={() => <DefaultTabBar />}
     >
       <Text tabLabel='Tab #1'>My</Text>
       <Text tabLabel='Tab #2'>favorite</Text>
       <Text tabLabel='Tab #3'>project</Text>
-    </ScrollableTabView>;
-  },
+    </ScrollableTabView>
+    );
+  }
 });
